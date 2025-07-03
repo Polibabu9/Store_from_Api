6817c67215610a2ee0edf8a7cd782fec2db53a39
Polibabu9 <addalapoli1@gmail.com>
2025-07-03 21:21:07 +0530

    some changes

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { Link , useParams } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function ProductDetails() {
-
+  const navigate = useNavigate();
   const {id} = useParams();
   const [product,setProduct] = useState(null);
 
@@ -18,15 +19,17 @@ function ProductDetails() {
 
   return (
     <>
-    <div style={{padding:20 ,display:'grid'}}>
-      <Link to="/">
-      <button>Back</button>
-      </Link>
-      <img src={product.image} alt={product.title}  height={400} width={300} style={{objectFit:'contain'}}/>
-      <h1>{product.title}</h1>
-      <p>{product.description}</p>
-      <h4>{product.price}</h4>
-      <p>Ratings : {product.rating.rate} count: {product.rating.count}</p>
+    <button onClick={()=> navigate('/')} style={{width:50 , padding:5}}>Back</button>
+    <div style={{padding:20 ,display:'flex'}}>
+      <div style={{flex:1 ,textAlign:'center'}}>
+        <img src={product.image} alt={product.title}  height={400} width={300} style={{objectFit:'contain' ,marginTop:20}}/>
+      </div>
+      <div style={{flex:1}}>
+        <h1>{product.title}</h1>
+        <p>{product.description}</p>
+        <h2>Price: ${product.price}</h2>
+        <p>Ratings : {product.rating.rate} count: {product.rating.count}</p>
+      </div>
     </div>
     </>
   )
