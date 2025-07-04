4fa24c7be2efc667033e68517fe882edf3580d28
Polibabu9 <addalapoli1@gmail.com>
2025-07-04 07:40:43 +0530

    loading change

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -3,19 +3,20 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { Link , useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import LoadingSpinner from './LoadingSpinner';
 
 function ProductDetails() {
   const navigate = useNavigate();
   const {id} = useParams();
-  const [product,setProduct] = useState(null);
+  const [product,setProduct] = useState('');
 
   useEffect(()=>{
     fetch(`https://fakestoreapi.com/products/${id}`)
     .then(res => res.json())
     .then(json => setProduct(json));
-  },[id]);
+  },[]);
 
-  if(!product) return <p> Loading....... </p>;
+  if(!product) return <LoadingSpinner />;
 
   return (
     <>
--- src/Components/Productcard.jsx
+++ src/Components/Productcard.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 function Productcard(props) {
+  const navigate = useNavigate();
   return (
     <>
-    <div style={{width:200 ,height: 300, textAlign: 'center' , padding:10 , border:'1px solid #cce' }}>
+    <div style={{width:200 ,height: 350, textAlign: 'center' , padding:10 , border:'1px solid #cce' }}>
       <img src={props.product.image} alt="" height={100} width={100} style={{objectFit:'contain'}}/>
       <h4>{props.product.title}</h4>
       <p>${props.product.price}</p>
-        <Link to={`/product/${props.product.id}`}>
-          <button>  View Details </button>
-        </Link>
+      <button onClick={()=> navigate(`/product/${props.product.id}`)} style={{padding:5}}> View Details</button>
     </div>
     </>
   )
