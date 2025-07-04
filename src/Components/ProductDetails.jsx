2eead031cd087b6203a6a7a2ad4a547d1e509464
Polibabu9 <addalapoli1@gmail.com>
2025-07-04 07:41:47 +0530

    link

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Link , useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import LoadingSpinner from './LoadingSpinner';
 
